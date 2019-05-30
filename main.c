79f4c03760681d70faf6c72bcefa03fd64d190e9
nicolaschillemi <nicolaschillemi@hotmail.com>
2019-05-30 19:39:28 -0300

    Ejercicio 100 guia practica

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+
+    int n1 = 0, n2 = 0, impares = 0, i = 0;
+
+    printf("Ingrese el rango de n%cmeros para contar los impares separados con un espacio: ",163);
+    scanf("%i %i",&n1,&n2);
+
+    for (i=n1-1;i<n2+1;i++) {
+        if (i % 2 != 0) {
+            impares++;
+        }
+    }
+
+    printf("%i",impares);
+
+}
